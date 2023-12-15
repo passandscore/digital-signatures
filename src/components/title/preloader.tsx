c926777fbe2dc52709ed2c82a89c4dd4fc84e197
Jason Schwarz <jason.c.schwarz@gmail.com>
2023-12-15 14:42:37 -0400

    Update Metadata

--- src/components/title/preloader.tsx
+++ src/components/title/preloader.tsx
@@ -1,38 +1,31 @@
-import { Center, Flex } from "@chakra-ui/react";
+import React from "react";
 import Image from "next/image";
 
 const Preloader = () => {
   return (
-    <Center h="75vh">
-      <Flex direction="column" align="center" justify="center">
-        <h1
-          style={{
-            fontSize: "2rem",
-            color: "white",
-            fontWeight: "bold",
-          }}
-        >
-          Digital Signatures
-        </h1>
-        <h2
-          style={{
-            fontSize: "1.2rem",
-            color: "#4299E1",
-            fontWeight: "bold",
-          }}
-        >
-          Signing & Verifying
-        </h2>
-
-        <Image
-          src="/preloader.gif"
-          alt="preloader"
-          priority
-          width={100}
-          height={25}
-        />
-      </Flex>
-    </Center>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "75vh",
+      }}
+    >
+      <h1 style={{ fontSize: "2rem", color: "white", fontWeight: "bold" }}>
+        Digital Signatures
+      </h1>
+      <h2 style={{ fontSize: "1.2rem", color: "#4299E1", fontWeight: "bold" }}>
+        Signing & Verifying
+      </h2>
+      {/* <Image
+        src="/preloader.gif"
+        alt="preloader"
+        priority
+        width={100}
+        height={15}
+      /> */}
+    </div>
   );
 };
 
